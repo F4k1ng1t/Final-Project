d071f83cd56a42e3d1a113d9051fa5eac336f79a
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2024-12-10 19:42:40 -0700

    AddedMoreMenuStuff
    Made a couple lines in the menu code that lets you select single or multi player

--- FinalProject/Assets/Scripts/Menu.cs
+++ FinalProject/Assets/Scripts/Menu.cs
@@ -29,153 +29,170 @@ public class Menu : MonoBehaviour/*MonoBehaviourPunCallbacks, ILobbyCallbacks*/
     public GameObject roomButtonPrefab;
     private List<GameObject> roomButtons = new List<GameObject>();
     // private List<RoomInfo> roomList = new List<RoomInfo>();
+   
+   
+   [Header("Single or Multi Player")]
+   public GameObject singlePlayerScreen;
+   public GameObject multiPlayercreen;
     /*
-    void Start()
-    {
-        createRoomButton.interactable = false;
-        findRoomButton.interactable = false;
+  void OnSinglePlayerButton()
+  {
+      SetScreen(singlePlayerScreen);
+  }
 
-        Cursor.lockState = CursorLockMode.None;
-
-        if (PhotonNetwork.InRoom)
-        {
-            PhotonNetwork.CurrentRoom.IsVisible = true;
-            PhotonNetwork.CurrentRoom.IsOpen = true;
-        }
-
-    }
-
-    void SetScreen(GameObject screen)
-    {
-
-        mainScreen.SetActive(false);
-        createRoomScreen.SetActive(false);
-        lobbyScreen.SetActive(false);
-        lobbyBrowserScreen.SetActive(false);
-
-        screen.SetActive(true);
-
-        if (screen == lobbyBrowserScreen)
-            UpdateLobbyBrowserUI();
-
-    }
-
-    public void OnPlayerNameValueChanged(TMP_InputField playerNameInput)
-    {
-        PhotonNetwork.NickName = playerNameInput.text;
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        createRoomButton.interactable = true;
-        findRoomButton.interactable = true;
-    }
-
-    public void OnCreateRoomButton()
-    {
-        SetScreen(createRoomScreen);
-    }
-
-    public void OnFindRoomButton()
-    {
-        SetScreen(lobbyBrowserScreen);
-    }
-
-    public void OnBackButton()
-    {
-        SetScreen(mainScreen);
-    }
-
-    public void OnCreateButton(TMP_InputField roomNameInput)
-    {
-        NetworkManager.instance.CreateRoom(roomNameInput.text);
-    }
-
-    public override void OnJoinedRoom()
-    {
-        SetScreen(lobbyScreen);
-        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
-    }
-
-    [PunRPC]
-    void UpdateLobbyUI()
-    {
-
-        startGameButton.interactable = PhotonNetwork.IsMasterClient;
-
-        playerListText.text = "";
-        foreach (Player player in PhotonNetwork.PlayerList)
-            playerListText.text += player.NickName + "\n";
-
-        roomInfoText.text = "<b>Room Name</b>\n" + PhotonNetwork.CurrentRoom.Name;
-    }
-
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        UpdateLobbyUI();
-    }
-
-    public void OnStartGameButton()
-    {
-
-        PhotonNetwork.CurrentRoom.IsOpen = false;
-        PhotonNetwork.CurrentRoom.IsVisible = false;
-
-        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
-    }
-
-    public void OnLeaveLobbyButton()
-    {
-        PhotonNetwork.LeaveRoom();
-        SetScreen(mainScreen);
-    }
-    void UpdateLobbyBrowserUI()
-    {
-
-        foreach (GameObject button in roomButtons)
-            button.SetActive(false);
-
-        for (int x = 0; x < roomList.Count; ++x)
-        {
-
-            GameObject button = x >= roomButtons.Count ? CreateRoomButton() : roomButtons[x];
-            button.SetActive(true);
-
-            button.transform.Find("RoomNameText").GetComponent<TextMeshProUGUI>().text =
-           roomList[x].Name;
-            button.transform.Find("PlayerCountText").GetComponent<TextMeshProUGUI>().text
-            = roomList[x].PlayerCount + " / " + roomList[x].MaxPlayers;
+  void OnMultiPlayerButton()
+  {
+      SetScreen(multiPlayerScreen);
+  }
 
 
-            Button buttonComp = button.GetComponent<Button>();
-            string roomName = roomList[x].Name;
-            buttonComp.onClick.RemoveAllListeners();
-            buttonComp.onClick.AddListener(() => { OnJoinRoomButton(roomName); });
-        }
-    }
 
-    GameObject CreateRoomButton()
-    {
-        GameObject buttonObj = Instantiate(roomButtonPrefab, roomListContainer.transform)
-       ;
-        roomButtons.Add(buttonObj);
-        return buttonObj;
-    }
+  void Start()
+  {
+      createRoomButton.interactable = false;
+      findRoomButton.interactable = false;
 
-    public void OnJoinRoomButton(string roomName)
-    {
-        NetworkManager.instance.JoinRoom(roomName);
-    }
+      Cursor.lockState = CursorLockMode.None;
 
-    public void OnRefreshButton()
-    {
-        UpdateLobbyBrowserUI();
-    }
+      if (PhotonNetwork.InRoom)
+      {
+          PhotonNetwork.CurrentRoom.IsVisible = true;
+          PhotonNetwork.CurrentRoom.IsOpen = true;
+      }
 
-    public override void OnRoomListUpdate(List<RoomInfo> allRooms)
-    {
-        roomList = allRooms;
-    }
-     */
+  }
+
+  void SetScreen(GameObject screen)
+  {
+
+      mainScreen.SetActive(false);
+      createRoomScreen.SetActive(false);
+      lobbyScreen.SetActive(false);
+      lobbyBrowserScreen.SetActive(false);
+
+      screen.SetActive(true);
+
+      if (screen == lobbyBrowserScreen)
+          UpdateLobbyBrowserUI();
+
+  }
+
+  public void OnPlayerNameValueChanged(TMP_InputField playerNameInput)
+  {
+      PhotonNetwork.NickName = playerNameInput.text;
+  }
+
+  public override void OnConnectedToMaster()
+  {
+      createRoomButton.interactable = true;
+      findRoomButton.interactable = true;
+  }
+
+  public void OnCreateRoomButton()
+  {
+      SetScreen(createRoomScreen);
+  }
+
+  public void OnFindRoomButton()
+  {
+      SetScreen(lobbyBrowserScreen);
+  }
+
+  public void OnBackButton()
+  {
+      SetScreen(mainScreen);
+  }
+
+  public void OnCreateButton(TMP_InputField roomNameInput)
+  {
+      NetworkManager.instance.CreateRoom(roomNameInput.text);
+  }
+
+  public override void OnJoinedRoom()
+  {
+      SetScreen(lobbyScreen);
+      photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+  }
+
+  [PunRPC]
+  void UpdateLobbyUI()
+  {
+
+      startGameButton.interactable = PhotonNetwork.IsMasterClient;
+
+      playerListText.text = "";
+      foreach (Player player in PhotonNetwork.PlayerList)
+          playerListText.text += player.NickName + "\n";
+
+      roomInfoText.text = "<b>Room Name</b>\n" + PhotonNetwork.CurrentRoom.Name;
+  }
+
+  public override void OnPlayerLeftRoom(Player otherPlayer)
+  {
+      UpdateLobbyUI();
+  }
+
+  public void OnStartGameButton()
+  {
+
+      PhotonNetwork.CurrentRoom.IsOpen = false;
+      PhotonNetwork.CurrentRoom.IsVisible = false;
+
+      NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+  }
+
+  public void OnLeaveLobbyButton()
+  {
+      PhotonNetwork.LeaveRoom();
+      SetScreen(mainScreen);
+  }
+  void UpdateLobbyBrowserUI()
+  {
+
+      foreach (GameObject button in roomButtons)
+          button.SetActive(false);
+
+      for (int x = 0; x < roomList.Count; ++x)
+      {
+
+          GameObject button = x >= roomButtons.Count ? CreateRoomButton() : roomButtons[x];
+          button.SetActive(true);
+
+          button.transform.Find("RoomNameText").GetComponent<TextMeshProUGUI>().text =
+         roomList[x].Name;
+          button.transform.Find("PlayerCountText").GetComponent<TextMeshProUGUI>().text
+          = roomList[x].PlayerCount + " / " + roomList[x].MaxPlayers;
+
+
+          Button buttonComp = button.GetComponent<Button>();
+          string roomName = roomList[x].Name;
+          buttonComp.onClick.RemoveAllListeners();
+          buttonComp.onClick.AddListener(() => { OnJoinRoomButton(roomName); });
+      }
+  }
+
+  GameObject CreateRoomButton()
+  {
+      GameObject buttonObj = Instantiate(roomButtonPrefab, roomListContainer.transform)
+     ;
+      roomButtons.Add(buttonObj);
+      return buttonObj;
+  }
+
+  public void OnJoinRoomButton(string roomName)
+  {
+      NetworkManager.instance.JoinRoom(roomName);
+  }
+
+  public void OnRefreshButton()
+  {
+      UpdateLobbyBrowserUI();
+  }
+
+  public override void OnRoomListUpdate(List<RoomInfo> allRooms)
+  {
+      roomList = allRooms;
+  }
+   */
 }
 
