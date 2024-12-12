0a6c46fffdaf5148f55623cba996a998fd552ce3
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2024-12-11 20:36:53 -0700

    AddedSomeLeaderboardStuff

--- FinalProject/Assets/Scripts/PlayerInfo.cs
+++ FinalProject/Assets/Scripts/PlayerInfo.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
+
 using PlayFab;
 using PlayFab.ClientModels;
-*/
+
 
 
 
@@ -12,10 +12,10 @@ public class PlayerInfo : MonoBehaviour
 {
 
     [HideInInspector]
-   // public PlayerProfileModel profile;
+    public PlayerProfileModel profile;
 
     public static PlayerInfo instance;
-    /*
+    
     void Awake() { instance = this; }
 
     public void OnLoggedIn()
@@ -39,5 +39,5 @@ public class PlayerInfo : MonoBehaviour
 );
 
     }
-    */
+    
 }
